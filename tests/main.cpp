4de5e1d2f1c2d6059ed840c00fa6112f6cae0f96
Alan Grover <alan.grover@us.af.mil>
2018-09-11 12:31:31 -0600

    added test cases for setting 8 and 16 bits

--- tests/main.cpp
+++ tests/main.cpp
@@ -112,15 +112,6 @@ TEST_CASE("Get enum fields.", "[getBits]") {
     REQUIRE(getBits<4, 8, Ustate>(src) == Ustate::ON);
 }
 
-TEST_CASE("type of ValueIntType made from uint64_t", "[uint64_t]") {
-    uint64_t src = 1;
-    REQUIRE(sizeof(src) * bits::BITS_IN_BYTE == 64);
-    REQUIRE(src << 63 == 0x8000000000000000);
-    typename std::make_signed<uint64_t>::type signed_src = 1;
-    REQUIRE(sizeof(signed_src) * bits::BITS_IN_BYTE == 64);
-    REQUIRE(static_cast<uint64_t>(signed_src << 63) == 0x8000000000000000);
-}
-
 TEST_CASE("Get from 64-bit src.", "[getBits]") {
     uint64_t src = 0xA000000000000F45;
     REQUIRE(getSbits<3, 61>(src) == -3);
@@ -129,14 +120,89 @@ TEST_CASE("Get from 64-bit src.", "[getBits]") {
     REQUIRE(getBits<4, 8, Ustate>(src) == Ustate::ON);
 }
 
-TEST_CASE("Get from an 8-bit src.", "[getBits]"){
-    uint8_t src = 0x45;
+TEST_CASE("Get from a 16-bit src.", "[getBits]"){
+    uint16_t src = 0xA045;
+    REQUIRE(getSbits<3, 13>(src) == -3);
     REQUIRE(getSbits<3, 0>(src) == -3);
     REQUIRE(getUbits<3, 0>(src) == 5);
     REQUIRE(getBits<4, 0, State>(src) == State::CA);
     REQUIRE(getBits<4, 0, Ustate>(src) == Ustate::CA);
 }
 
+TEST_CASE("Get from an 8-bit src.", "[getBits]"){
+    uint8_t src = 0x45;
+    REQUIRE(getSbits<3, 0>(src) == -3);
+    REQUIRE(getUbits<3, 0>(src) == 5);
+    REQUIRE(getSbits<3, 5>(src) == 2);
+    REQUIRE(getBits<4, 0, State>(src) == State::CA);
+    REQUIRE(getBits<4, 0, Ustate>(src) == Ustate::CA);
+}
 
+TEST_CASE("Set three fields, dest = 0, uint64_t.", "[setBits]") {
+    uint64_t dest = 0;
+    setBits<3, 61>(dest, 0xFFFFFFFFFFFFFFFF);
+    REQUIRE(dest == 0xE000000000000000);
+    int32_t value = -1;
+    setBits<3, 0>(dest, value);
+    REQUIRE(dest == 0xE000000000000007);
+    setBits<4, 5>(dest, State::SuperconductiveAtRoomTemperature);
+    REQUIRE(dest == 0xE000000000000147);
+}
+
+TEST_CASE("Set three fields, dest = 0xFFFFFFFFFFFFFFFF, uint64_t.", "[setBits]") {
+    uint64_t dest = 0xFFFFFFFFFFFFFFFF;
+    setBits<3, 61>(dest, 0);
+    REQUIRE(dest == 0x1FFFFFFFFFFFFFFF);
+    setBits<3, 2>(dest, 0);
+    REQUIRE(dest == 0x1FFFFFFFFFFFFFE3);
+    int32_t value = -4;
+    setBits<3, 0>(dest, value);
+    REQUIRE(dest == 0x1FFFFFFFFFFFFFE4);
+}
+
+TEST_CASE("Set three fields, dest = 0, uint8_t.", "[setBits]") {
+    uint8_t dest = 0;
+    setBits<3, 5>(dest, 0xFF);
+    REQUIRE(dest == 0xE0);
+    int32_t value = -1;
+    setBits<3, 0>(dest, value);
+    REQUIRE(dest == 0xE7);
+    setBits<4, 3>(dest, State::SuperconductiveAtRoomTemperature);
+    REQUIRE(dest == 0xD7);
+}
+
+TEST_CASE("Set three fields, dest = 0xFF, uint8_t.", "[setBits]") {
+    uint8_t dest = 0xFF;
+    setBits<3, 5>(dest, 0);
+    REQUIRE(dest == 0x1F);
+    setBits<3, 2>(dest, 0);
+    REQUIRE(dest == 0x03);
+    int32_t value = -4;
+    setBits<3, 0>(dest, value);
+    REQUIRE(dest == 0x04);
+}
+
+TEST_CASE("Set three fields, dest = 0, uint16_t.", "[setBits]") {
+    uint16_t dest = 0;
+    setBits<3, 13>(dest, 0xFFFF);
+    REQUIRE(dest == 0xE000);
+    int32_t value = -1;
+    setBits<3, 0>(dest, value);
+    REQUIRE(dest == 0xE007);
+    setBits<4, 3>(dest, State::SuperconductiveAtRoomTemperature);
+    REQUIRE(dest == 0xE057);
+}
+
+
+TEST_CASE("Set three fields, dest = 0xFFFF, uint16_t.", "[setBits]") {
+    uint16_t dest = 0xFFFF;
+    setBits<3, 13>(dest, 0);
+    REQUIRE(dest == 0x1FFF);
+    setBits<3, 2>(dest, 0);
+    REQUIRE(dest == 0x1FE3);
+    int32_t value = -4;
+    setBits<3, 0>(dest, value);
+    REQUIRE(dest == 0x1FE4);
+}
 
 
